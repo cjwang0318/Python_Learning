3e3d6ba9ce7d1f0999328d3de5e03a1fb00fcce7
cjwang <cjwang0318@gmail.com>
2023-08-18 23:33:34 +0800

    update paddle_formate_transform

--- paddle_formate_transform.py
+++ paddle_formate_transform.py
@@ -1,20 +1,22 @@
 import json
 from tqdm import tqdm
+import cv2
 def write_file(path, write_data):
     with open(path, 'w', encoding='utf-8') as writer:
         writer.writelines(write_data)
 
 
-def det_format(f):
+def det_format(f, path):
     data = json.load(f)
-    output_list = []
+    output_det_list = []
+    output_rec_list = []
     for fineName in tqdm(data):
         # print(data[fineName]["ann"])
         for dict in data[fineName]["ann"]:
             if dict["cls"] == "exp":
                 result_dict = {}
-                point_list = []
-                result_dict["transcription"] = dict["transcription"]
+                transcription=dict["transcription"]
+                result_dict["transcription"] = transcription
                 bbox_list = dict["bbox"]
                 x1 = int(bbox_list[0])
                 y1 = int(bbox_list[1])
@@ -31,8 +33,15 @@ def det_format(f):
                 result_dict["points"] = [point1, point2, point3, point4]
                 jsondict = json.dumps(result_dict)
                 #print(f"{fineName}\t{jsondict}")
-                output_list.append(f"{fineName}\t[{jsondict}]\n")
-    return output_list
+                output_det_list.append(f"{fineName}\t[{jsondict}]\n")
+                #crap image for recogination
+                img = cv2.imread(f"{path}/images/{fineName}")
+                crop_img = img[y1:y3, x1:x3]
+                #cv2.imshow("cropped", crop_img)
+                #cv2.waitKey(0)
+                cv2.imwrite(f"{path}/images_paddle/{fineName}", crop_img)
+                output_rec_list.append(f"{fineName}\t{transcription}\n")
+    return output_det_list, output_rec_list
 
 
 
@@ -41,7 +50,8 @@ if __name__ == '__main__':
     path="D:/temp/Products-Real/evaluation/"
     # Opening JSON file
     f = open(path+'annotations.json')
-    output_list=det_format(f)
-    write_file(path+"paddle_det.txt",output_list)
+    output_dec_list, output_rec_list=det_format(f, path)
+    write_file(path+"paddle_det.txt",output_dec_list)
+    write_file(path + "paddle_rec.txt", output_rec_list)
     # Closing file
     f.close()
