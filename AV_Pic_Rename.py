db8e918a7f95233fbf04b60108f1787957500a75
cjwang <cjwang0318@gmail.com>
2023-09-29 20:52:43 +0800

    update AV_Pic path problem

--- AV_Pic_Rename.py
+++ AV_Pic_Rename.py
@@ -1,30 +1,33 @@
 import os
+
 if __name__ == '__main__':
-    #dirPath = r"Y:/BT/JAV_output"
-    # 專案路徑
+     # 專案路徑
     project_dir = 'Y:/BT/JAV_output/'
-    #project_dir = './JAV_output/'
+    # project_dir = './JAV_output/'
     source = os.walk(project_dir)
     for folder, subfolders, filenames in source:
-        #print(f'目前資料夾路徑為：{folder}')
-        for subfolder in subfolders:
-            print(f'{folder}的子資料夾為：{subfolder}')
+        print(f'目前資料夾路徑為：{folder}')
         for filename in filenames:
-            print(f'{folder}/{subfolder}內含檔案為：{filename}')
-            current_path=f'{folder}/'
-            current_path=current_path.replace("\\","/")
-            #print("current_path:"+current_path)
-            #rename thumb.jpg to folder_name.jpg
-            if filename =="thumb.jpg":
-                sourceName=f"{current_path}/thumb.jpg"
-                targetName=f"{current_path}/{subfolder}.jpg"
-                #print(sourceName)
-                #print(targetName)
+            currentPath = os.path.join(folder, filename)
+            currentPath = currentPath.replace('\\', '/')
+            print(f"currentPath={currentPath}")
+            # print(os.path.join(folder, filename))
+            # rename thumb.jpg to folder_name.jpg
+            if filename == "thumb.jpg":
+                subfolder = currentPath.rsplit('/')[-2]
+                sourceName = os.path.join(folder, "thumb.jpg")
+                sourceName = sourceName.replace('\\', '/')
+                targetName = os.path.join(folder, f"{subfolder}.jpg")
+                targetName = targetName.replace('\\', '/')
+                # print(sourceName)
+                # print(targetName)
+                print(f"修改檔名 thumb.jpg -> {subfolder}.jpg")
                 os.rename(sourceName, targetName)
             if filename == "poster.jpg":
-                targetName = f"{current_path}poster.jpg"
+                targetName = currentPath
+                print(f"刪除 poster.jpg -> {targetName}")
                 os.remove(targetName)
             if ".nfo" in filename:
-                targetName = f"{current_path}{filename}"
-                #print(targetName)
+                targetName = currentPath
+                print(f"刪除 .nfo -> {targetName}")
                 os.remove(targetName)
